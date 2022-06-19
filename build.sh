9c9bd735b76284aa84297538d8787dd88a280d8b
marmooo <marmooo@users.noreply.github.com>
2022-06-20 08:38:07 +0900

    drop-inline-css

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 mkdir -p docs
 cp -r src/* docs
+drop-inline-css -i src/小1/一/index.html > inline.css
+drop-inline-css -r src -o docs -c inline.css
+drop-inline-css src/index.html -o docs/index.html
+rm inline.css
 minify -r docs -o .
