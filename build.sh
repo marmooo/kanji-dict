5270354d8907e082b18970efc0c108e9ad8ead7b
marmooo <marmooo@users.noreply.github.com>
2023-12-10 17:47:20 +0900

    update

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ drop-inline-css -i src/小1/一/index.html > inline.css
 drop-inline-css -r src -o docs -c inline.css
 drop-inline-css -i src/部首/一部/index.html > inline.css
 drop-inline-css -r src/部首 -o docs/部首 -c inline.css
+drop-inline-css -i src/画数/25画〜/index.html > inline.css
+drop-inline-css -r src/画数 -o docs/画数 -c inline.css
 drop-inline-css src/index.html -o docs/index.html
 rm inline.css
 deno run -A bundle.js ./src/index.js > docs/index.js
