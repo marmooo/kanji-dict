c1145c335e878b68032630cca842a7468961a3c3
marmooo <marmooo@users.noreply.github.com>
2024-01-03 00:26:26 +0900

    fix drop-inline-css

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ drop-inline-css -i src/小1/一/index.html > inline.css
 drop-inline-css -r src -o docs -c inline.css
 drop-inline-css -i src/部首/一部/index.html > inline.css
 drop-inline-css -r src/部首 -o docs/部首 -c inline.css
-drop-inline-css -i src/画数/25画〜/index.html > inline.css
+drop-inline-css -i src/画数/1画/index.html > inline.css
 drop-inline-css -r src/画数 -o docs/画数 -c inline.css
 drop-inline-css -i src/unicode/URO1/index.html > inline.css
 drop-inline-css -r src/unicode -o docs/unicode -c inline.css
