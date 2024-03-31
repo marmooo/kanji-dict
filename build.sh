47d2b57d31e82a5c852cc0f13f85243089132642
marmooo <marmooo@users.noreply.github.com>
2024-03-31 12:28:02 +0900

    bump drop-inline-css from 0.2.1 to 0.3.1

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ deno task radical-html
 deno task strokes-html
 deno task unicode-html
 cp -r src/* docs
-drop-inline-css -i src/小1/一/index.html > inline.css
-drop-inline-css -r src -o docs -c inline.css
-drop-inline-css -i src/部首/一部/index.html > inline.css
-drop-inline-css -r src/部首 -o docs/部首 -c inline.css
-drop-inline-css -i src/画数/1画/index.html > inline.css
-drop-inline-css -r src/画数 -o docs/画数 -c inline.css
-drop-inline-css -i src/unicode/URO1/index.html > inline.css
-drop-inline-css -r src/unicode -o docs/unicode -c inline.css
-drop-inline-css src/index.html -o docs/index.html
-drop-inline-css src/glyph/index.html -o docs/glyph/index.html
+deno run -A ~/workspace/drop-inline-css/cli.js -d src/小1/一/index.html > inline.css
+deno run -A ~/workspace/drop-inline-css/cli.js -r src -o docs -c inline.css
+deno run -A ~/workspace/drop-inline-css/cli.js -d src/部首/一部/index.html > inline.css
+deno run -A ~/workspace/drop-inline-css/cli.js -r src/部首 -o docs/部首 -c inline.css
+deno run -A ~/workspace/drop-inline-css/cli.js -d src/画数/1画/index.html > inline.css
+deno run -A ~/workspace/drop-inline-css/cli.js -r src/画数 -o docs/画数 -c inline.css
+deno run -A ~/workspace/drop-inline-css/cli.js -d src/unicode/URO1/index.html > inline.css
+deno run -A ~/workspace/drop-inline-css/cli.js -r src/unicode -o docs/unicode -c inline.css
+deno run -A ~/workspace/drop-inline-css/cli.js src/index.html -o docs/index.html
+deno run -A ~/workspace/drop-inline-css/cli.js src/glyph/index.html -o docs/glyph/index.html
 rm inline.css
 deno run -A bundle.js ./src/index.js > docs/index.js
 minify -r docs -o .
