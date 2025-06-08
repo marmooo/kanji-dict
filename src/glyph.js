dc1d8a28c95d635f648892c0a38beaf1e6ac70ef
marmooo <marmooo@users.noreply.github.com>
2025-06-08 14:55:28 +0900

    update

--- src/glyph.js
+++ src/glyph.js
@@ -323,6 +323,28 @@ function addReferences(kanji) {
   ul[4].appendChild(getUnicodeReferences(kanji)); // Unicode
 }
 
+function getStrokesComponent(strokesText) {
+  const components = strokesText.split(" ").map((str) => {
+    const strokes = Number(str);
+    const text = (strokes != 0) ? `${strokes}画` : "";
+    const dir = (strokes >= 25) ? `25画〜` : `${strokes}画`;
+    return `<a href="/kanji-dict/画数/${dir}/">${text}</a>`;
+  });
+  return components.join(" または ");
+}
+
+function getRadicalComponent(radicalText) {
+  const radicals = Array.from(
+    "一丨丶丿乙亅二亠人儿入八冂冖冫几凵刀力勹匕匚匸十卜卩厂厶又口囗土士夂夊夕大女子宀寸小尢尸屮山巛工己巾干幺广廴廾弋弓彐彡彳心戈戶手支攴文斗斤方无日曰月木欠止歹殳毋比毛氏气水火爪父爻爿片牙牛犬玄玉瓜瓦甘生用田疋疒癶白皮皿目矛矢石示禸禾穴立竹米糸缶网羊羽老而耒耳聿肉臣自至臼舌舛舟艮色艸虍虫血行衣襾見角言谷豆豕豸貝赤走足身車辛辰辵邑酉釆里金長門阜隶隹雨靑非面革韋韭音頁風飛食首香馬骨高髟鬥鬯鬲鬼魚鳥鹵鹿麥麻黃黍黑黹黽鼎鼓鼠鼻齊齒龍龜龠",
+  );
+  const components = radicalText.split(" ").map((str) => {
+    const id = Number(str);
+    const text = `${radicals[id]}部`;
+    return `<a href="/kanji-dict/部首/${text}/">${text}</a>`;
+  });
+  return components.join(" または ");
+}
+
 function addKanjiInfo(kanji, hex, csv) {
   const arr = csv.split(",");
   const grade = Number(arr[4]);
@@ -334,15 +356,8 @@ function addKanjiInfo(kanji, hex, csv) {
   trs[3].children[1].textContent = jkatNames[grade];
   trs[4].children[1].textContent = arr[5].replace(/ /g, ","); // 音読み
   trs[5].children[1].textContent = arr[6].replace(/ /g, ","); // 訓読み
-  const strokes = Number(arr[7]);
-  if (strokes != 0) {
-    const strokesText = `${strokes}画`;
-    const strokesDir = (strokes < 25) ? strokesText : "25画〜";
-    const strokesURL = `/kanji-dict/画数/${strokesDir}/`;
-    trs[6].children[1].appendChild(getLink(strokesURL, strokesText));
-  }
-  const componentURL = `/kanji-dict/部首/${arr[8]}/`;
-  trs[7].children[1].appendChild(getLink(componentURL, arr[8]));
+  trs[6].children[1].innerHTML = getStrokesComponent(arr[7]);
+  trs[7].children[1].innerHTML = getRadicalComponent(arr[8]);
   trs[8].children[1].textContent = arr[9]; // 部首
   const examples = document.getElementById("examples");
   const divs = examples.querySelectorAll("div");
