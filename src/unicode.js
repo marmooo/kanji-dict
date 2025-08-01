5d5ea4d28826ae80355329377f32d30ef09a2d3d
marmooo <marmooo@users.noreply.github.com>
2025-08-01 21:41:04 +0900

    fix unicode table

--- src/unicode.js
+++ src/unicode.js
@@ -25,30 +25,16 @@ function checkCharacters(chars) {
   });
 }
 
-function colorTable(td, result) {
-  return new Promise((resolve) => {
-    if (td.classList.contains("bg-warning-subtle")) {
-      resolve();
-    } else {
-      if (!result.supported) {
-        td.classList.add("bg-warning-subtle");
-      }
-      resolve();
-    }
-  });
-}
-
 function isElementInViewport(node) {
   const { top, bottom } = node.getBoundingClientRect();
   return 0 <= bottom && top <= globalThis.innerHeight;
 }
 
 async function checkSupportInViewport() {
+  if (running) return;
+  running = true;
   const trs = codeTrs.filter((tr) => isElementInViewport(tr));
-  const id = Date.now();
-  scrollId = id;
   for (const tr of trs) {
-    if (id != scrollId) continue;
     const tds = tr.children;
     const hex = tds[0].textContent;
     const from = Number(`0x${hex}`);
@@ -56,16 +42,26 @@ async function checkSupportInViewport() {
     const chars = codes.map((code) => String.fromCodePoint(code));
     const results = await checkCharacters(chars);
     await Promise.all(
-      results.map((result, i) => colorTable(tds[i + 1], result)),
+      results.map((result, i) => {
+        const td = tds[i + 1];
+        if (!td.classList.contains("bg-warning-subtle") && !result.supported) {
+          td.classList.add("bg-warning-subtle");
+        }
+      }),
     );
   }
+  running = false;
 }
 
 loadConfig();
+let scrollTimeout;
+let running = false;
 const worker = new Worker("/kanji-dict/unicode-worker.js", { type: "module" });
-let scrollId;
 const codeTrs = [...document.querySelectorAll("#table tr")].slice(1);
 checkSupportInViewport();
 
 document.getElementById("toggleDarkMode").onclick = toggleDarkMode;
-globalThis.addEventListener("scroll", checkSupportInViewport);
+globalThis.addEventListener("scroll", () => {
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(checkSupportInViewport, 100);
+});
